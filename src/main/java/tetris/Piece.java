177b943b6ad64dd9e734376ef584a2e972417537
Sergio Laguna <serg.o.34@gmail.com>
2016-12-12 19:50:07 +0100

    Refactor class Piece

--- src/main/java/tetris/Piece.java
+++ src/main/java/tetris/Piece.java
@@ -6,13 +6,34 @@ package tetris;
 
 public class Piece {
 
-    private String piece;
+    private int rows;
+    private int columns;
+    private char[][] piece;
 
     public Piece(String piece) {
-        this.piece = piece;
+        String[] str_rows = piece.split("\n");
+        this.rows = str_rows.length;
+        this.columns = str_rows[0].length();
+        this.piece = new char[this.rows][this.columns];
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                this.piece[i][j] = str_rows[i].charAt(j);
+            }
+        }
     }
 
     public String toString() {
-        return this.piece;
+        String ps = "";
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++) {
+                ps += this.piece[i][j];
+            }
+            ps += "\n";
+        }
+        return ps;
+    }
+
+    public Piece rotateRight() {
+        return this;
     }
 }
