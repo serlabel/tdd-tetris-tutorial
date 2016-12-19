9e6c7f71ba03661a8af309539ed17a11ba740e2a
Sergio Laguna <serg.o.34@gmail.com>
2016-12-19 20:31:34 +0100

    Pass test 8 of RotatingTetrominoesTest
    RotatingTetrominoesTest: The_I_shape.can_be_rotated_left_once

--- src/main/java/tetris/Tetromino.java
+++ src/main/java/tetris/Tetromino.java
@@ -9,22 +9,24 @@ public class Tetromino {
     public final static Tetromino T_SHAPE = new Tetromino("" +
             ".T.\n" +
             "TTT\n" +
-            "...\n");
+            "...\n", 4);
     public final static Tetromino I_SHAPE = new Tetromino("" +
             ".....\n" +
             ".....\n" +
             "IIII.\n" +
             ".....\n" +
-            ".....\n");
+            ".....\n", 2);
 
     private Piece[] shapes;
     private int actual_rotation;
+    private int num_rotations;
 
-    public Tetromino(String shape) {
+    public Tetromino(String shape, int n_rotations) {
         this.actual_rotation = 0;
-        shapes = new Piece[4];
+        this.num_rotations = n_rotations;
+        shapes = new Piece[n_rotations];
         this.shapes[0] = new Piece(shape);
-        for(int i=1; i < 4; i++) {
+        for(int i=1; i < n_rotations; i++) {
             this.shapes[i] = this.shapes[i-1].rotateRight();
         }
     }
@@ -32,6 +34,7 @@ public class Tetromino {
     private Tetromino(Piece[] rotations, int actual_rotation) {
         this.shapes = rotations;
         this.actual_rotation = actual_rotation;
+        this.num_rotations = rotations.length;
     }
 
     public String toString() {
@@ -39,11 +42,13 @@ public class Tetromino {
     }
 
     public Tetromino rotateRight() {
-        return new Tetromino(this.shapes, (this.actual_rotation + 1) % 4);
+        return new Tetromino(this.shapes,
+                (this.actual_rotation + 1) % this.num_rotations);
     }
 
     public Tetromino rotateLeft() {
-        return new Tetromino(this.shapes, (this.actual_rotation + 3) % 4);
+        return new Tetromino(this.shapes,
+                (this.actual_rotation + this.num_rotations - 1) % this.num_rotations);
     }
 
 }
