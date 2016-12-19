737efa706adaee4c23d88294e0a3f855b6fd77a5
Sergio Laguna <serg.o.34@gmail.com>
2016-12-19 20:23:19 +0100

    Pass test 7 of RotatingTetrominoesTest
    RotatingTetrominoesTest: The_I_shape.can_be_rotated_right_once

--- src/test/java/tetris/Step3_RotatingTetrominoesTest.java
+++ src/test/java/tetris/Step3_RotatingTetrominoesTest.java
@@ -122,16 +122,16 @@ public class Step3_RotatingTetrominoesTest extends Assert {
                     ".....\n", shape.toString());
         }
 
-//        @Test
-//        public void can_be_rotated_right_once() {
-//            shape = shape.rotateRight();
-//            assertEquals("" +
-//                    "..I..\n" +
-//                    "..I..\n" +
-//                    "..I..\n" +
-//                    "..I..\n" +
-//                    ".....\n", shape.toString());
-//        }
+        @Test
+        public void can_be_rotated_right_once() {
+            shape = shape.rotateRight();
+            assertEquals("" +
+                    "..I..\n" +
+                    "..I..\n" +
+                    "..I..\n" +
+                    "..I..\n" +
+                    ".....\n", shape.toString());
+        }
 
 //        @Test
 //        public void can_be_rotated_left_once() {
