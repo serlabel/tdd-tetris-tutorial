2d0b331473ace17566c4f990c46210dc3a7282d3
Sergio Laguna <serg.o.34@gmail.com>
2017-01-02 17:21:37 +0100

    Pass test 11 of RotatingTetrominoesTest
    RotatingTetrominoesTest: The_O_shape.cannot_be_rotated_right

--- src/test/java/tetris/Step3_RotatingTetrominoesTest.java
+++ src/test/java/tetris/Step3_RotatingTetrominoesTest.java
@@ -170,14 +170,14 @@ public class Step3_RotatingTetrominoesTest extends Assert {
                     "...\n", shape.toString());
         }
 
-//        @Test
-//        public void cannot_be_rotated_right() {
-//            shape = shape.rotateRight();
-//            assertEquals("" +
-//                    ".OO\n" +
-//                    ".OO\n" +
-//                    "...\n", shape.toString());
-//        }
+        @Test
+        public void cannot_be_rotated_right() {
+            shape = shape.rotateRight();
+            assertEquals("" +
+                    ".OO\n" +
+                    ".OO\n" +
+                    "...\n", shape.toString());
+        }
 
 //        @Test
 //        public void cannot_be_rotated_left() {
